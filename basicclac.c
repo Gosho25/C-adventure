82b7e05eec09c5e909987816e1975a17dc10b2c1
Bat_Gosho <goshkodark25@gmail.com>
2024-12-26 22:16:35 +0200

    Create basicclac.c

---
+++ basicclac.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    float n1;
+    float n2;
+    scanf("%c %f %f", &op, &n1, &n2);
+    float res;
+    if(op == '+')
+    {
+        res = n1 + n2;
+    }
+    else if(op == '-')
+    {
+        res = n1 - n2;
+    }
+    else if(op == '*')
+    {
+        res = n1 * n2;
+    }
+    else if(op == '/')
+    {
+        res = n1 / n2;
+    }
+
+    printf("%f", res);
+    return 0;
+}
