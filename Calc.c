a82ce1feaa3fedb14c9e9eaaedecd6746b5bbaa7
Bat_Gosho <goshkodark25@gmail.com>
2024-12-29 21:59:44 +0200

    Create Calc.c

---
+++ Calc.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    float num1;
+    float num2;
+    char operation;
+    float result;
+    while(1)
+    {
+        scanf("%c %f %f", &operation, &num1, &num2);
+        if(operation == 'e')
+        {
+            break;
+        }
+        else if(operation == '+')
+        {
+            result = num1 + num2;
+        }
+        else if(operation == '-')
+        {
+            result = num1 - num2;
+        }        
+        else if(operation == '*')
+        {
+            result = num1 * num2;
+        }
+        else if(operation == '/')
+        {
+            result = num1 / num2;
+        }
+    }
+    printf("%.6f\n", result);
+    return 0;
+};
