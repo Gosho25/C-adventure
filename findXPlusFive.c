db6de02622a3417fb4c54bc745b38d00416d9b30
Bat_Gosho <goshkodark25@gmail.com>
2025-01-06 17:36:48 +0200

    Create findXPlusFive.c

---
+++ findXPlusFive.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+    // Don't change above this line
+
+    int x = 0;
+    if(num > 5)
+    {
+        x += 5;
+    }
+    
+    // Don't change below this line
+    printf("x = %d", x);
+    return 0;
+}
