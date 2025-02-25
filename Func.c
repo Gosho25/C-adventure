d0c356e6d34b3ef78fcce6dcebb6e3815358f266
Bat_Gosho <goshkodark25@gmail.com>
2025-02-25 14:55:56 +0200

    Create Func.c

---
+++ Func.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+void returner()
+{
+    printf("Hello\n");
+    printf("Methods\n");
+}
+
+
+static int multiplier(int num)
+{
+    return num;
+    /*    
+    int mult;
+    printf("Write a multiplier: \n");
+    scanf_s("%d ", &mult);
+    int result = num * mult;
+    */
+}
+
+int main() 
+{
+    returner();
+    multiplier(2);
+    return 0;
+}
