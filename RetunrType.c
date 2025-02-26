4bff93dba5d2fc1e1cd1968b9936d2cf74e28d20
Bat_Gosho <goshkodark25@gmail.com>
2025-02-26 17:53:06 +0200

    Create RetunrType.c

---
+++ RetunrType.c
@@ -0,0 +1,17 @@
+static int multiplier(int num1, int num2)
+{
+    int result = num1 * num2;
+    printf("The sum of %d and %d is %d", num1, num2, result);
+    return result;
+}
+
+
+
+
+int main() 
+{
+    printf("\n");
+    int funcObject = multiplier(2, 2);//imame printf vuv funkciqta i e izlishen v main
+    printf("\n");
+    return 0;
+}
