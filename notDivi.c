8b3198a7eaf870835d5ee159f4a632ca88112aa2
Bat_Gosho <goshkodark25@gmail.com>
2025-03-12 19:14:09 +0200

    Update notDivi.c

--- notDivi.c
+++ notDivi.c
@@ -5,7 +5,10 @@ int main() {
     scanf("%d", &num);
     for(int i = 1;i <= num;i++)
     {
-        if(i % 3 == 0 || i % 7 == 0) continue;
+        if(i % 3 == 0 || i % 7 == 0)
+        {
+            continue;
+        }
         printf("%d\n", i);
     }
     return 0;
